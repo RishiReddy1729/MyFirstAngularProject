ef3544824aefed38c8b5851a7380a344a3d22cc8
mrreddy@gn.com <mrreddy@gn.com>
2024-03-09 18:26:02 +0530

    uninstalled slick carousel

--- src/app/body-content/body-content.component.ts
+++ src/app/body-content/body-content.component.ts
@@ -1,13 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component, OnInit} from '@angular/core';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 
 @Component({
   selector: 'app-body-content',
   standalone: true,
-  imports: [SlickCarouselModule,
-  NgFor],
+  imports: [],
   templateUrl: './body-content.component.html',
   styleUrl: './body-content.component.css'
 })
@@ -17,17 +15,6 @@ export class BodyContentComponent implements OnInit{
   
   constructor(){}
   ngOnInit(): void { }
-  images = [
-    {img: "https://www.india.gov.in/sites/upload_files/npi/files/ecisveep.jpg"},
-    {img: "https://cbpssubscriber.mygov.in/assets/uploads/flYHd7E7b2W2XRtP?7"},
-  ];
-
-  slickConfig = {
-    "infinite": true,
-    "slidesToShow": 3,
-    "slidesToScroll": 3,
-    "autoplay": true,
-    "autoplaySpeed": 2000,
-    "arrows": true
-    };
+  
+  
 }
